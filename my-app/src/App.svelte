11dbb8c195e3a51ee705ec56294dc4e65a14c664
Jeff <stramoine7@gmail.com>
2022-06-04 23:11:04 +0200

    ok

---
+++ my-app/src/App.svelte
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../public/css/style.css">
+<script>
+  let name = 'jeff'
+  import Other from "./Other.svelte"
+</script>
+
+<main>
+  <h1>Hello {name}</h1>
+  <Other/>
+</main>
+
